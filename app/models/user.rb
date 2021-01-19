0a23e0b034ef06d7af303152e22a8bc727384775
makuto114 <35193508+makuto114@users.noreply.github.com>
2021-01-19 12:43:04 +0900

    /registrations/new.html.erbの編集　
    rb:4, rb:14、及びインプットエリアを編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validates :nickname, presence: true
+  # validates :email, presence: true, uniqueness: true, 
+  #           format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+            
+
+  # validates :password, presence: true, length: { minimum: 7 },
+  #           format: { with: }
+
 end
