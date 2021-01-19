0eea1bfed12879e013f2da2353ea92b306810ba4
makuto114 <35193508+makuto114@users.noreply.github.com>
2021-01-19 14:52:49 +0900

    ユーザコントローラー　editとcreateアクションの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    if @user.create
+      render root_path
+    else
+      render action: :new
   end
 end
