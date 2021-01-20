fef0efc10a185f84d13109c5f3469dfb54049773
makuto114 <35193508+makuto114@users.noreply.github.com>
2021-01-20 14:20:17 +0900

    itemモデル　バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  validates :name, :introduction, :price, :user, :category_id, :condition_id, :postage_id, :shipping_days_id, :prefecture_id, presence: true
+  
+  belongs_to :user
+  has_one    :order
 end
