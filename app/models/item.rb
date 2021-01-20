4d32de3d6564935212c1b1bdafd4bcdbf7c3572d
makuto114 <35193508+makuto114@users.noreply.github.com>
2021-01-20 17:43:16 +0900

    Active Storage導入　アソシエーションまで

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
+  belongs_to       :user
+  has_one          :order
+  has_one_attached :image
+  
   validates :name, :introduction, :price, :user, :category_id, :condition_id, :postage_id, :shipping_days_id, :prefecture_id, presence: true
   
-  belongs_to :user
-  has_one    :order
+  
 end
