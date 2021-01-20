5bb40796a25ea7d9c0aa6159927c8ceb082c1b01
makuto114 <35193508+makuto114@users.noreply.github.com>
2021-01-20 17:40:56 +0900

    ActiveHash用　モデル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,12 +4,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new
+    if @item.save
+      render root_path
+    else
+      render action: :new
+    end
   end
 
   def show
   end
-
+  
+  private 
+  
 end
